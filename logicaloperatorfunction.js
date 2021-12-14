504639b19f4269c2e2cba23d03f7922640efad01
Mounika <bojjamounika8@gmail.com>
2021-12-15 00:05:30 +0530

    logicaloperators

---
+++ logicaloperatorfunction.js
@@ -0,0 +1,30 @@
+
+if (20<30 && 30<40 && 40<50){
+    console.log(true)
+}else{
+    console.log(false)
+} 
+
+
+if (20<30 || 30>40 || 40>50){
+    console.log(true)
+}else{
+    console.log(false)
+} 
+
+if(20>30){
+    console.log("Wrong statement");
+}else if (20<30){
+    console.log("Right statement");
+}else {
+     console.log("choose another statement");
+  }
+
+if(20>30){
+      console.log("Wrong statement");
+  }else if (20>30){
+      console.log("Right statement");
+  }else {
+      console.log("choose another statement");
+  }
+
